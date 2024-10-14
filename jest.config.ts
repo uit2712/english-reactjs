2bd3225412fe43481e01fb59bbb14473b63045ab
uit2712 <phamquangvi456@gmail.com>
2024-10-14 04:03:47 +0000

    update

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,7 @@ const config: Config = {
     verbose: true,
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleNameMapper: {
-        '^@/core/(.*)$': '<rootDir>/src/core/$1',
-        '^@/framework/(.*)$': '<rootDir>/src/framework/$1',
+        '^@/(.*)$': '<rootDir>/src/$1',
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     },
     moduleDirectories: ['node_modules', '<rootDir>/'],
